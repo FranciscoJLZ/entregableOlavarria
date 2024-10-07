16b37a8f1e9839e5cd6943062e47d0b40f509fb5
FranciscoJLZ <165858949+FranciscoJLZ@users.noreply.github.com>
2024-10-07 17:03:03 -0300

    Add files via upload

---
+++ reCamiones.ts
@@ -0,0 +1,35 @@
+import { Autos } from "./reAutos";
+
+export class Camiones extends Autos{
+    private arrCamiones: Camiones[]=[];
+    public constructor(marca: string, modelo: string, valor: string) {
+        super(marca,modelo,valor);
+    }
+    //------
+    public agregarCamion(marca:string,modelo:string,valor:string) {
+        const nuevoCamion = new Camiones(marca,modelo,valor);
+        this.arrCamiones.push(nuevoCamion);
+    }
+    //-----
+    public modificarCamion(nuevaMarca:string, nuevoModelo: string, nuevoValor: string,i: number) {
+        if(i>=0 && i< this.arrCamiones.length) {
+            const camion: Autos = this.arrCamiones[i];
+            camion.setMarca(nuevaMarca);
+            camion.setModelo(nuevoModelo);
+            camion.setValor(nuevoValor);
+            console.log("Se ha modificado el vehiculo con exito.")
+        }else{
+            console.log("Iterador inválido.")
+        }
+    }
+    //----
+    public darDeBaja(i: number): void {
+        if (i >= 0 && i < this.arrCamiones.length) {
+            this.arrCamiones.splice(i, 1);
+            console.log("Se ha dado de baja el camion numero: ", i);
+        } else {
+            console.log("Iterador inválido.");
+        }
+    }
+
+}
